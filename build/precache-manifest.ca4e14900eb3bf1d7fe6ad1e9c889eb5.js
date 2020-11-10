f9889ee43e9c0e8d03122f7265537e230168c832
Heder Felipe <hederfelipe@gmail.com>
2020-11-10 13:10:54 -0300

    finalfinal

---
+++ build/precache-manifest.ca4e14900eb3bf1d7fe6ad1e9c889eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb4520367ced4a02a0c7ae1f4bbe682",
+    "url": "/index.html"
+  },
+  {
+    "revision": "899401d6e5f79443db7e",
+    "url": "/static/css/main.0bc2c517.chunk.css"
+  },
+  {
+    "revision": "0fdbfc233b448ec6566c",
+    "url": "/static/js/2.ddd51adf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ddd51adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "899401d6e5f79443db7e",
+    "url": "/static/js/main.abcd4ac4.chunk.js"
+  },
+  {
+    "revision": "818cc6ca9d0bcea6cba0",
+    "url": "/static/js/runtime-main.abf1af40.js"
+  }
+]);
